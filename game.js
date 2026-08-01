8c0c5382fb095c4ffadc72dc83b1701c3741c17a
analista1 <analistaprogramador1@escafandra.com.co>
2026-08-01 15:54:06 -0500

    Paso 3

--- game.js
+++ game.js
@@ -8,6 +8,30 @@ const state = {
   blocks: [],
 };
 
+const paddle = {
+  x: 195, y: 570, w: 90, h: 14, speed: 6,
+};
+
+const keys = {};
+
+function movePaddle() {
+  if ( ( keys[ 'ArrowLeft' ] || keys[ 'a' ] || keys[ 'A' ] ) ) {
+    paddle.x -= paddle.speed;
+  }
+  if ( ( keys[ 'ArrowRight' ] || keys[ 'd' ] || keys[ 'D' ] ) ) {
+    paddle.x += paddle.speed;
+  }
+  if ( paddle.x < 0 ) paddle.x = 0;
+  if ( paddle.x + paddle.w > canvas.width ) paddle.x = canvas.width - paddle.w;
+}
+
+function drawPlayingScreen() {
+  ctx.fillStyle = '#000';
+  ctx.fillRect( 0, 0, canvas.width, canvas.height );
+  movePaddle();
+  drawSprite( ctx, 'paddle', paddle.x, paddle.y, paddle.w, paddle.h );
+}
+
 function drawStartScreen() {
   ctx.fillStyle = '#000';
   ctx.fillRect( 0, 0, canvas.width, canvas.height );
@@ -20,6 +44,8 @@ function drawStartScreen() {
 function update() {
   if ( state.screen === 'START' ) {
     drawStartScreen();
+  } else if ( state.screen === 'PLAYING' ) {
+    drawPlayingScreen();
   }
 }
 
@@ -28,10 +54,15 @@ function loop() {
   requestAnimationFrame( loop );
 }
 
-window.addEventListener( 'keydown', () => {
+window.addEventListener( 'keydown', ( e ) => {
+  keys[ e.key ] = true;
   if ( state.screen === 'START' ) {
     state.screen = 'PLAYING';
   }
+} );
+
+window.addEventListener( 'keyup', ( e ) => {
+  keys[ e.key ] = false;
 } );
 
 loadSpritesheet( () => {
